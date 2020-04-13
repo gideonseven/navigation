3e59133a28017e3fa675df1e680c7b1fd58a3144
GIDEON\don <gideon.tobing@hotmail.com>
2020-04-13 08:41:28 +0700

    initiate default template

--- app/src/main/java/com/don/navigation/MainActivity.kt
+++ app/src/main/java/com/don/navigation/MainActivity.kt
@@ -2,11 +2,21 @@ package com.don.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(savedInstanceState == null){
+            setupBottomNavigation()
+        }
     }
+
+    private fun setupBottomNavigation() {}
+
 }
