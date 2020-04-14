c1a4a6f25963e108407bc22dc96671d4ec4700b0
GIDEON\don <gideon.tobing@hotmail.com>
2020-04-14 10:43:59 +0700

    setup initial

--- app/src/main/java/com/don/navigation/MainActivity.kt
+++ app/src/main/java/com/don/navigation/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.don.navigation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.LiveData
-import androidx.navigation.NavController
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(savedInstanceState == null){
-            setupBottomNavigation()
-        }
-    }
 
-    private fun setupBottomNavigation() {}
+    }
 
 }
