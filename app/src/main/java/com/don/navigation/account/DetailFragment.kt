9e3eab52094b69f9685177bde73dcf8faa54d0d2
GIDEON\don <gideon.tobing@hotmail.com>
2020-04-17 12:53:31 +0700

    passing bundle safeargs

--- app/src/main/java/com/don/navigation/account/DetailFragment.kt
+++ app/src/main/java/com/don/navigation/account/DetailFragment.kt
@@ -5,34 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 
 import com.don.navigation.R
-import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class AccountFragment : Fragment() {
+class DetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
 
+
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val name = arguments?.let { DetailFragmentArgs.fromBundle(it).name }
 
-        btnDetailAccount.setOnClickListener {
-            val action =
-                AccountFragmentDirections.actionAccountScreenToDetailFragment(etName.text.toString())
-
-            findNavController().navigate(action)
-        }
+        tvDetail.text = "Halo " + name
     }
 
 }
